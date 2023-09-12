203a80eb1e38e614b8f6e31b8cef57ccb2c2b028
Kouno-metoro <KounoMetoro_0000@outlook.jp>
2023-09-12 22:09:18 +0900

    #includeを追加

---
+++ LED_Tokyu_5050.c
@@ -0,0 +1,14 @@
+// g++cmd:g++ -o led_06 led_06.c -Wall -I /home/metoro/rpi-rgb-led-matrix/include -L /home/metoro/rpi-rgb-led-matrix/lib -lrgbmatrix -I /home/metoro/stb -lwiringPi -g -O0
+
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <stdio.h>        //標準入出力
+#include <stdlib.h>       //メモリ関係
+#include <dirent.h>       //ディレクトリ関係
+#include <string.h>       //文字列関係
+#include <errno.h>        //エラー関係
+#include <led-matrix-c.h> //パネル制御
+#include <stb_image.h>    //画像を読み込む
+#include <unistd.h>       //sleep()用
+#include <wiringPi.h>     //delay()用
+
